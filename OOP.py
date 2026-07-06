22dbb0a0848e3872fcd04e46cc280d95b837db3b
Daksh Joshi <joshidaksh085@gmail.com>
2026-07-06 14:17:28 +0530

    concept

---
+++ OOP.py
@@ -0,0 +1,6 @@
+# Class--> blueprint
+#  Objects--> Entities
+
+# Class is a blueprint for creating objects. It defines a set of attributes and methods that the created objects will have. Objects are instances of classes, representing specific entities with their own unique data and behavior.
+# Objects are created from classes and can have their own state (attributes) and behavior (methods). Each object can have different values for its attributes, allowing for the creation of multiple instances of the same class with varying characteristics.
+

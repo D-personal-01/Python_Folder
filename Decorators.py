1a5c3882083a38452b901b836d6cb7a8eef42f6c
Daksh Joshi <joshidaksh085@gmail.com>
2026-07-07 13:19:17 +0530

    trying diffrent way

--- Decorators.py
+++ Decorators.py
@@ -1,4 +1,4 @@
-# Decorators modifies a function
+# Decorators are used to modify a function.
 
 def greet(fx):
     def dfx(*a, **b):
@@ -9,6 +9,15 @@ def greet(fx):
         print ("Thanks for using this function")
         print ()
     return dfx
+# We can use it like this
+def addition(a,b):
+    print ("Sum of both the terms are-\n",a+b)
+    return
+
+greet(addition(2,5))
+greet(addition(234,348))
+
+# But instad of repeating again and again we can use this.
 
 @greet
 def addition(a,b):
@@ -16,3 +25,4 @@ def addition(a,b):
     return
 
 addition(2**14281,16**3571)
+
